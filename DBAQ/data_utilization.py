0eee9dc9787152ce5cd3c780b40b7a5f47a767c4
shubh-soubhagya <soubhagyasrivastava240@gmail.com>
2025-03-20 10:31:16 +0530

    Updated funtion

--- DBAQ/data_utilization.py
+++ DBAQ/data_utilization.py
@@ -1,21 +1,29 @@
+import os
+import logging
 from langchain_community.document_loaders import PyPDFDirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
+from dotenv import load_dotenv
 
 
+def load_environment():
+    load_dotenv()
+    logging.getLogger("langchain").setLevel(logging.ERROR)
+    return os.getenv('GROQ_API_KEY')
 
-def load_pdfs(pdf_path):
-    loader = PyPDFDirectoryLoader(pdf_path)
+def load_and_split_pdfs(pdf_directory, chunk_size=500, chunk_overlap=0):
+    loader = PyPDFDirectoryLoader(pdf_directory)
     docs = loader.load()
     print(f"✅ Loaded {len(docs)} documents successfully.")
-    return docs
 
-def split_documents(docs, chunk_size=500, chunk_overlap=0):
-    documents = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap).split_documents(docs)
+    documents = RecursiveCharacterTextSplitter(
+        chunk_size=chunk_size, chunk_overlap=chunk_overlap
+    ).split_documents(docs)
     print(f"✅ Successfully split into {len(documents)} text chunks.")
     return documents
 
-def create_vectorstore(documents, model_path):
-    embeddings_model = HuggingFaceEmbeddings(model_name=model_path)
-    return FAISS.from_documents(documents, embeddings_model)
+def create_vector_db(documents, embeddings_path):
+    embeddings_model = HuggingFaceEmbeddings(model_name=embeddings_path)
+    vectordb = FAISS.from_documents(documents, embeddings_model)
+    return vectordb.as_retriever()
